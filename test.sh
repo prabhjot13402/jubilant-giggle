b161338869867b0693fbdba0f9ea08cff163de1f
prabhjot13402 <103111592+prabhjot13402@users.noreply.github.com>
2024-12-30 13:42:08 +0530

    Delete test.sh

--- test.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-for s in $(cat variables.json | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]" ); do
-    echo $s
-    sed -i -e 's/'$s'/'$s'/g' .env
-    echo "hello"
-    echo "there"
-    echo "branch"
-	echo "Updated"
-done
